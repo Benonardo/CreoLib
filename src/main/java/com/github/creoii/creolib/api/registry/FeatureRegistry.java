a1496279350de2507a4d5d4dd94fde1f478110d8
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-03-22 18:04:38 -0400

    Remove Deformed Circle Feature

--- src/main/java/com/github/creoii/creolib/api/registry/FeatureRegistry.java
+++ src/main/java/com/github/creoii/creolib/api/registry/FeatureRegistry.java
@@ -11,14 +11,12 @@ import net.minecraft.world.gen.feature.Feature;
 public final class FeatureRegistry {
     public static final Feature<CompositeFeatureConfig> COMPOSITE = new CompositeFeature(CompositeFeatureConfig.CODEC);
     public static final Feature<CompositeSelectorFeatureConfig> COMPOSITE_SELECTOR = new CompositeSelectorFeature(CompositeSelectorFeatureConfig.CODEC);
-    public static final Feature<DeformedCircleFeatureConfig> DEFORMED_CIRCLE = new DeformedCircleFeature(DeformedCircleFeatureConfig.CODEC);
     public static final Feature<PaintSurfaceFeatureConfig> PAINT_SURFACE = new PaintSurfaceFeature(PaintSurfaceFeatureConfig.CODEC);
     public static final Feature<PoolFeatureConfig> POOL = new PoolFeature(PoolFeatureConfig.CODEC);
 
     public static void register() {
         Registry.register(Registries.FEATURE, new Identifier(CreoLib.NAMESPACE, "composite"), COMPOSITE);
         Registry.register(Registries.FEATURE, new Identifier(CreoLib.NAMESPACE, "composite_selector"), COMPOSITE_SELECTOR);
-        Registry.register(Registries.FEATURE, new Identifier(CreoLib.NAMESPACE, "deformed_circle"), DEFORMED_CIRCLE);
         Registry.register(Registries.FEATURE, new Identifier(CreoLib.NAMESPACE, "paint_surface"), PAINT_SURFACE);
         Registry.register(Registries.FEATURE, new Identifier(CreoLib.NAMESPACE, "pool"), POOL);
     }
