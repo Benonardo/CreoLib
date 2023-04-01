309fafd7893591b34b7c3f2cfdbf43b28cc57211
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-04-01 15:51:51 -0400

    CraterFeature from 23w13a_or_b
    Cause' we know damn well theyre not gonna keep this in the game

--- src/main/java/com/github/creoii/creolib/api/registry/FeatureRegistry.java
+++ src/main/java/com/github/creoii/creolib/api/registry/FeatureRegistry.java
@@ -13,11 +13,13 @@ public final class FeatureRegistry {
     public static final Feature<CompositeSelectorFeatureConfig> COMPOSITE_SELECTOR = new CompositeSelectorFeature(CompositeSelectorFeatureConfig.CODEC);
     public static final Feature<PoolFeatureConfig> POOL = new PoolFeature(PoolFeatureConfig.CODEC);
     public static final Feature<StructureFeatureConfig> STRUCTURE = new StructureFeature(StructureFeatureConfig.CODEC);
+    public static final Feature<CraterFeatureConfig> CRATER = new CraterFeature(CraterFeatureConfig.CODEC);
 
     public static void register() {
         Registry.register(Registries.FEATURE, new Identifier(CreoLib.NAMESPACE, "composite"), COMPOSITE);
         Registry.register(Registries.FEATURE, new Identifier(CreoLib.NAMESPACE, "composite_selector"), COMPOSITE_SELECTOR);
         Registry.register(Registries.FEATURE, new Identifier(CreoLib.NAMESPACE, "pool"), POOL);
         Registry.register(Registries.FEATURE, new Identifier(CreoLib.NAMESPACE, "structure"), STRUCTURE);
+        Registry.register(Registries.FEATURE, new Identifier(CreoLib.NAMESPACE, "crater"), CRATER);
     }
 }
